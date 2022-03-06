e723de532da56017e565b2ac70e260a0d979b103
itsmohiful <mohiful.islam.ofc@gmail.com>
2022-03-06 15:11:43 +0600

    html file to pdf

---
+++ pdf/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.accept,name='accept'),
+    path('<int:id>/',views.resume,name='resume'),
+    path('list/',views.cv_list,name='cv_list'),
+]
